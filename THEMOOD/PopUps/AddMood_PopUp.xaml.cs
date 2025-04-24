0329bc7c9fac18382979c1afef62c838e28a58cf
Hans Borsigue <Hans Borsigue@DESKTOP-A1U1LCV>
2025-04-24 22:52:18 +0800

    update popup undone

--- THEMOOD/PopUps/AddMood_PopUp.xaml.cs
+++ THEMOOD/PopUps/AddMood_PopUp.xaml.cs
@@ -1,14 +1,54 @@
 using CommunityToolkit.Maui.Views;
 using THEMOOD.ViewModels;
 using THEMOOD.Services;
+using THEMOOD.Converters;
 
 namespace THEMOOD.PopUps;
 
 public partial class AddMood_PopUp : Popup
 {
+    private bool _isSavedEnabled;
+    public bool IsSavedEnabled
+    {
+        get => _isSavedEnabled;
+        set
+        {
+            if (_isSavedEnabled != value)
+            {
+                _isSavedEnabled = value;
+                OnPropertyChanged(nameof(IsSavedEnabled));
+            }
+        }
+    }
+
     public AddMood_PopUp()
     {
         InitializeComponent();
+
+        IsSavedEnabled = false;
+        SaveButton.IsEnabled = false;
+        SaveButton.BindingContext = this;
+
+        MoodDatePicker.DateSelected += ValidateForm;
+        MoodPicker.SelectedIndexChanged += ValidateForm;
+        MoodNotesEntry.TextChanged += ValidateForm;
+    }
+
+    public void ValidateForm(object sender, EventArgs e)
+    {
+        ValidateForm();
+    }
+
+    private void ValidateForm()
+    {
+        // Check if all fields are filled
+        bool isDateSelected = MoodDatePicker.Date != DateTime.MinValue;
+        bool isMoodSelected = MoodPicker.SelectedIndex != -1;
+        bool isNoteEntered = !string.IsNullOrWhiteSpace(MoodNotesEntry.Text);
+
+        // Enable Save button if all fields are filled
+        IsSavedEnabled = isDateSelected && isMoodSelected && isNoteEntered;
+        SaveButton.IsEnabled = IsSavedEnabled;
     }
 
     private void CancelButton_Clicked(object sender, EventArgs e)
@@ -18,6 +58,9 @@ public partial class AddMood_PopUp : Popup
 
     private void SaveButton_Clicked(object sender, EventArgs e)
     {
+        if(!IsSavedEnabled)
+            return;
+        
         // Extract mood without emoji first
         string moodText = string.Empty;
         var selected = MoodPicker.SelectedItem?.ToString();
